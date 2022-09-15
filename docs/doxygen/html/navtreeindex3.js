c7d2e9efe1f62d50cf25ca84b948dc7656e98cfb
Github <github@example.com>
2022-09-15 18:46:26 +0000

    Документация: Обновление документации с помощью doxygen [Автосборка]

---
+++ docs/doxygen/html/navtreeindex3.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX3 =
+{
+"files.html":[1,0],
+"globals.html":[1,1,0],
+"globals_func.html":[1,1,1],
+"index.html":[],
+"pages.html":[]
+};
---
+++ docs/doxygen/html/search/all_13.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['vbox_5fguest_5finfo_107',['vbox_guest_info',['../d5/d45/structvbox__guest__info.html',1,'']]],
+  ['vbox_5fheader_108',['vbox_header',['../d5/dbe/structvbox__header.html',1,'']]],
+  ['vfs_5ffilesystem_5fhandles_5ft_109',['vfs_filesystem_handles_t',['../d1/daa/structvfs__filesystem__handles__t.html',1,'']]],
+  ['vfs_5ffilesystem_5ft_110',['vfs_filesystem_t',['../d0/dad/structvfs__filesystem__t.html',1,'']]],
+  ['vfs_5fmount_5finfo_5ft_111',['vfs_mount_info_t',['../d4/d51/structvfs__mount__info__t.html',1,'']]],
+  ['vfs_5fstorage_5fdev_5ft_112',['vfs_storage_dev_t',['../da/deb/structvfs__storage__dev__t.html',1,'']]],
+  ['vga_5fentry_113',['vga_entry',['../dc/d03/tty_8c.html#ac3fd406df8ed388f03d8b94a340660b9',1,'tty.c']]]
+];
