44705b1025249d347a119fb0591ba013b91fd691
NDRAEY <andrejpavlenko666@gmail.com>
2022-09-19 22:05:10 +0900

    Tried to make the AC'97 driver, but I failed...

--- kernel/src/drivers/experimental/ac97.c
+++ kernel/src/drivers/experimental/ac97.c
@@ -7,6 +7,29 @@
 #include "../../../include/libk/stdio.h"
 #include <io/ports.h>
 
+#define RESET 0x00
+#define MASTER_VOL 0x02
+#define MIC_VOL 0x0E
+#define PCM_VOL 0x18
+#define SELECT_INPUT 0x1A
+#define SAMPLE_RATE_FRONT 0x2C
+
+struct MasterVol {
+	unsigned int right : 5;
+	unsigned int zero : 1;
+	unsigned int left : 5;
+	unsigned int zero2 : 1;
+	unsigned int mute : 1;
+};
+
+struct PCMVol {
+	unsigned int right : 4;
+	unsigned int zero : 2;
+	unsigned int left : 4;
+	unsigned int zero2 : 1;
+	unsigned int mute : 1;
+};
+
 unsigned int ac97_init() {
 	int retval = pci_read(
 		pci_get_device(0x8086, 0x2415, -1),
@@ -15,4 +38,24 @@ unsigned int ac97_init() {
 
 	qemu_log("[AC'97] Returned value: %d\n", retval);
 	return retval;
+}
+
+void ac97_test() {
+	struct MasterVol mast = { // Half of volume
+		0b111010,
+		0,
+		0b111010,
+		0,
+		0
+	};
+
+	struct PCMVol pcm = { // Half of volume
+		16,
+		0,
+		16,
+		0,
+		0
+	};
+
+	// XXX: NDRAEY NEEDS HELP (Minimal code to play the audio)
 }
