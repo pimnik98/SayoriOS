8050d4bd05c64fa23ef9382f9e00f2ce933680bc
Nikita Piminoff <48570109+pimnik98@users.noreply.github.com>
2022-11-19 22:46:26 +0300

    Delete kernel directory
    Signed-off-by: Nikita Piminoff <48570109+pimnik98@users.noreply.github.com>

--- kernel/include/io/colors.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define COLOR_TEXT      0xFFFFFF
-#define COLOR_SYS_TEXT  0x92D7D4
-#define COLOR_SYS_PATH  0x335190
-#define COLOR_ERROR     0xA5383B
-#define COLOR_ALERT     0xA66938
