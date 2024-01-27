ef1c822383ab28c236700e2e53b1903b00ef653c
NDRAEY <pikachu_andrey@vk.com>
2024-01-28 01:37:15 +0900

    Files: COmment test

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -525,15 +525,15 @@ int kernel(multiboot_header_t* mboot, uint32_t initial_esp) {
     
     hda_init();
 
-    {
-        ttf_font_t hah = {};
-
-        ttf_init(&hah, "R:\\font.ttf");
-
-        ttf_draw_string(&hah, (char *) getFrameBufferAddr(), getScreenWidth(), getScreenHeight(), 0, 0, "Cattiva");
-
-        ttf_destroy(&hah);
-    }
+//     {
+//         ttf_font_t hah = {};
+// 
+//         ttf_init(&hah, "R:\\font.ttf");
+// 
+//         ttf_draw_string(&hah, (char *) getFrameBufferAddr(), getScreenWidth(), getScreenHeight(), 0, 0, "Cattiva");
+// 
+//         ttf_destroy(&hah);
+//     }
 
     cli();
     
