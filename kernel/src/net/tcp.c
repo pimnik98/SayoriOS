9973a45f5397630d439a81de0c082411f2f9b166
NDRAEY <pikachu_andrey@vk.com>
2024-05-23 15:18:12 +0900

    Net/TCP: Detect payload size

--- kernel/src/net/tcp.c
+++ kernel/src/net/tcp.c
@@ -2,12 +2,18 @@
 // Created by ndraey on 21.4.2024.
 //
 
+#include "net/ethernet.h"
 #include "net/tcp.h"
 #include "net/endianess.h"
 #include "io/ports.h"
 
 void tcp_handle_packet(netcard_entry_t *card, tcp_packet_t *packet) {
     qemu_note("!!!!!!!!!!!!!!!!!!!!!!!! TCP !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+    ETH_IPv4_PKG *ipv4 = (ETH_IPv4_PKG *)((size_t)packet - sizeof(ETH_IPv4_PKG));
+    size_t data_payload_size = ipv4->TotalLength - sizeof(ETH_IPv4_PKG);
+
+    qemu_log("Data payload size: %d", data_payload_size);
 
     packet->source = ntohs(packet->source);
     packet->destination = ntohs(packet->destination);
