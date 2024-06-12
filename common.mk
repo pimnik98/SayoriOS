a33b15508a0a0bbc0ced5453e60615e0fecc3d29
NDRAEY <pikachu_andrey@vk.com>
2024-06-12 18:14:52 +0900

    Files: Build tweaks

--- common.mk
+++ common.mk
@@ -7,6 +7,7 @@ DEBUG?=# -ggdb3 #-Werror
 MEMORY_SIZE?=128M
 USE_SSE?=true
 OPTIMIZATION_LEVEL?=0
+BUILD_PREFIX=./
 
 COMPILER_DETECTOR_FLAGS = ""
 
@@ -14,197 +15,194 @@ ifeq ($(X64),true)
 	COMPILER_DETECTOR_FLAGS := "--64"
 endif
 
-CC=$(shell bash tools/compiler.sh $(COMPILER_DETECTOR_FLAGS))
+CC=$(shell bash $(BUILD_PREFIX)tools/compiler.sh $(COMPILER_DETECTOR_FLAGS))
 
 OBJ_DIRECTORY = objects
-DIRECTORIES = objects/kernel/src \
-				objects/kernel/asm \
-				objects/kernel/src/lib \
-				objects/kernel/src/lib/libstring/src \
-				objects/kernel/src/lib/libvector/src \
-				objects/kernel/src/lib/php \
-				objects/kernel/src/lib/elk \
-				objects/kernel/src/lib/elk/ext \
-				objects/kernel/src/extra \
-				objects/kernel/src/sys \
-				objects/kernel/src/io \
-				objects/kernel/src/net \
-				objects/kernel/src/drv \
-				objects/kernel/src/drv/disk \
-				objects/kernel/src/debug \
-				objects/kernel/src/fmt \
-				objects/kernel/src/drv/audio \
-				objects/kernel/src/drv/video \
-				objects/kernel/src/drv/input \
-				objects/kernel/src/drv/network \
-				objects/kernel/src/fs \
-				objects/kernel/src/lib/math \
-				objects/kernel/src/toys \
-				objects/kernel/src/gfx \
-				objects/kernel/src/gui \
-				objects/kernel/src/desktop \
-				objects/kernel/src/user \
-				objects/kernel/src/ports/gameboy \
-				objects/kernel/src/mem \
-				objects/kernel/src/arch/x86 \
+DIRECTORIES = $(BUILD_PREFIX)objects/kernel/src \
+				$(BUILD_PREFIX)objects/kernel/asm \
+				$(BUILD_PREFIX)objects/kernel/src/lib \
+				$(BUILD_PREFIX)objects/kernel/src/lib/libstring/src \
+				$(BUILD_PREFIX)objects/kernel/src/lib/libvector/src \
+				$(BUILD_PREFIX)objects/kernel/src/lib/php \
+				$(BUILD_PREFIX)objects/kernel/src/lib/elk \
+				$(BUILD_PREFIX)objects/kernel/src/lib/elk/ext \
+				$(BUILD_PREFIX)objects/kernel/src/extra \
+				$(BUILD_PREFIX)objects/kernel/src/sys \
+				$(BUILD_PREFIX)objects/kernel/src/io \
+				$(BUILD_PREFIX)objects/kernel/src/net \
+				$(BUILD_PREFIX)objects/kernel/src/drv \
+				$(BUILD_PREFIX)objects/kernel/src/drv/disk \
+				$(BUILD_PREFIX)objects/kernel/src/debug \
+				$(BUILD_PREFIX)objects/kernel/src/fmt \
+				$(BUILD_PREFIX)objects/kernel/src/drv/audio \
+				$(BUILD_PREFIX)objects/kernel/src/drv/video \
+				$(BUILD_PREFIX)objects/kernel/src/drv/input \
+				$(BUILD_PREFIX)objects/kernel/src/drv/network \
+				$(BUILD_PREFIX)objects/kernel/src/fs \
+				$(BUILD_PREFIX)objects/kernel/src/lib/math \
+				$(BUILD_PREFIX)objects/kernel/src/toys \
+				$(BUILD_PREFIX)objects/kernel/src/gfx \
+				$(BUILD_PREFIX)objects/kernel/src/gui \
+				$(BUILD_PREFIX)objects/kernel/src/desktop \
+				$(BUILD_PREFIX)objects/kernel/src/user \
+				$(BUILD_PREFIX)objects/kernel/src/ports/gameboy \
+				$(BUILD_PREFIX)objects/kernel/src/mem \
+				$(BUILD_PREFIX)objects/kernel/src/arch/x86 \
 				# objects/kernel/src/compress/zlib \
 
 # Исходные объектные модули
-ASM_SRC=kernel/asm/init.s \
-	kernel/asm/interrupt.s \
-	kernel/asm/sys_calls.s \
-	kernel/asm/usr.s \
-	kernel/asm/sse.s \
-	kernel/asm/gdt.s \
-	kernel/asm/paging.s \
-	kernel/asm/regs.s \
-	kernel/src/lib/setjmp.s \
-	kernel/asm/switch_task.s \
-	kernel/asm/64bit_on_32bit.s \
-#	kernel/src/sys/v8086.s \
+ASM_SRC=$(BUILD_PREFIX)kernel/asm/init.s \
+	$(BUILD_PREFIX)kernel/asm/interrupt.s \
+	$(BUILD_PREFIX)kernel/asm/sys_calls.s \
+	$(BUILD_PREFIX)kernel/asm/usr.s \
+	$(BUILD_PREFIX)kernel/asm/sse.s \
+	$(BUILD_PREFIX)kernel/asm/gdt.s \
+	$(BUILD_PREFIX)kernel/asm/paging.s \
+	$(BUILD_PREFIX)kernel/asm/regs.s \
+	$(BUILD_PREFIX)kernel/src/lib/setjmp.s \
+	$(BUILD_PREFIX)kernel/asm/switch_task.s \
+	$(BUILD_PREFIX)kernel/asm/64bit_on_32bit.s \
 
 ASM=$(ASM_SRC:%.s=$(OBJ_DIRECTORY)/%.o)
 
 SOURCES=\
-	kernel/src/sys/cpuinfo.c \
-	kernel/src/sys/cpu_isr.c \
-	kernel/src/sys/gdt.c \
-	kernel/src/sys/isr.c \
-	kernel/src/io/ports.c \
-	kernel/src/io/serial_port.c \
-	kernel/src/lib/string.c \
-	kernel/src/drv/fpu.c \
-	kernel/src/sys/timer.c \
-	kernel/src/sys/logo.c \
-	kernel/src/lib/math/math.c \
-	kernel/src/mem/pmm.c	\
-	kernel/src/mem/vmm.c	\
+	$(BUILD_PREFIX)kernel/src/sys/cpuinfo.c \
+	$(BUILD_PREFIX)kernel/src/sys/cpu_isr.c \
+	$(BUILD_PREFIX)kernel/src/sys/gdt.c \
+	$(BUILD_PREFIX)kernel/src/sys/isr.c \
+	$(BUILD_PREFIX)kernel/src/io/ports.c \
+	$(BUILD_PREFIX)kernel/src/io/serial_port.c \
+	$(BUILD_PREFIX)kernel/src/lib/string.c \
+	$(BUILD_PREFIX)kernel/src/drv/fpu.c \
+	$(BUILD_PREFIX)kernel/src/sys/timer.c \
+	$(BUILD_PREFIX)kernel/src/sys/logo.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/math.c \
+	$(BUILD_PREFIX)kernel/src/mem/pmm.c	\
+	$(BUILD_PREFIX)kernel/src/mem/vmm.c	\
 	$(wildcard kernel/src/lib/libvector/src/*.c) \
 	$(wildcard kernel/src/lib/libstring/src/*.c) \
-	kernel/src/lib/stdio.c \
-	kernel/src/lib/split.c \
-	kernel/src/io/screen.c \
-	kernel/src/io/tty.c \
-	kernel/src/fs/tarfs.c \
-	kernel/src/fs/fsm.c \
-	kernel/src/fs/nvfs.c \
-	kernel/src/fs/natfs.c \
-	kernel/src/fs/tempfs.c \
-	kernel/src/fs/iso9660.c \
-	kernel/src/lib/php/str_replace.c \
-	kernel/src/sys/scheduler.c \
-	kernel/src/lib/php/explode.c \
-	kernel/src/lib/php/pathinfo.c \
-	kernel/src/lib/elk/elk.c \
-	kernel/src/lib/elk/elk_engine.c \
-	kernel/src/lib/elk/elk_libs.c \
-	kernel/src/lib/elk/jse_func.c \
+	$(BUILD_PREFIX)kernel/src/lib/stdio.c \
+	$(BUILD_PREFIX)kernel/src/lib/split.c \
+	$(BUILD_PREFIX)kernel/src/io/screen.c \
+	$(BUILD_PREFIX)kernel/src/io/tty.c \
+	$(BUILD_PREFIX)kernel/src/fs/tarfs.c \
+	$(BUILD_PREFIX)kernel/src/fs/fsm.c \
+	$(BUILD_PREFIX)kernel/src/fs/nvfs.c \
+	$(BUILD_PREFIX)kernel/src/fs/natfs.c \
+	$(BUILD_PREFIX)kernel/src/fs/tempfs.c \
+	$(BUILD_PREFIX)kernel/src/fs/iso9660.c \
+	$(BUILD_PREFIX)kernel/src/lib/php/str_replace.c \
+	$(BUILD_PREFIX)kernel/src/sys/scheduler.c \
+	$(BUILD_PREFIX)kernel/src/lib/php/explode.c \
+	$(BUILD_PREFIX)kernel/src/lib/php/pathinfo.c \
+	$(BUILD_PREFIX)kernel/src/lib/elk/elk.c \
+	$(BUILD_PREFIX)kernel/src/lib/elk/elk_engine.c \
+	$(BUILD_PREFIX)kernel/src/lib/elk/elk_libs.c \
+	$(BUILD_PREFIX)kernel/src/lib/elk/jse_func.c \
 	$(wildcard kernel/src/lib/elk/ext/*.c) \
-	kernel/src/drv/psf.c \
-	kernel/src/sys/unwind.c \
-	kernel/src/fs/NatSuki.c \
-	kernel/src/drv/disk/initrd.c \
-	kernel/src/drv/disk/dpm.c \
-	kernel/src/lib/list.c \
-	kernel/src/lib/fileio.c \
-	kernel/src/sys/sync.c \
-	kernel/src/gui/basics.c \
-	kernel/src/lib/pixel.c \
-	kernel/src/sys/bootscreen.c \
-	kernel/src/debug/hexview.c \
-	kernel/src/drv/video/vbe.c \
-	kernel/src/drv/input/keyboard.c \
-	kernel/src/drv/input/mouse.c \
-	kernel/src/sys/syscalls.c \
-	kernel/src/sys/testing.c \
-	kernel/src/sys/trigger.c \
-	kernel/src/lib/rand.c \
-	kernel/src/drv/cmos.c \
-	kernel/src/drv/beeper.c \
-	kernel/src/user/env.c \
-	kernel/src/drv/pci.c \
-	kernel/src/gui/pointutils.c \
-	kernel/src/gui/line.c \
-	kernel/src/gui/circle.c \
-	kernel/src/lib/math/exp.c \
-	kernel/src/lib/math/log.c \
-	kernel/src/lib/math/pow.c \
-	kernel/src/lib/math/acos.c \
-	kernel/src/lib/math/asin.c \
-	kernel/src/lib/math/atan.c \
-	kernel/src/lib/math/modf.c \
-	kernel/src/lib/math/integral.c \
-	kernel/src/lib/math/sin.c \
-	kernel/src/lib/math/cos.c \
-	kernel/src/lib/math/tan.c \
-	kernel/src/lib/math/sqrt.c \
-	kernel/src/io/rgb_image.c \
-	$(wildcard kernel/src/lib/libstring/*.c) \
-	kernel/src/lib/math/cbrt.c \
-	kernel/src/sys/cpuid.c	\
-	kernel/src/drv/disk/ata.c \
-	kernel/src/drv/disk/atapi.c \
-	kernel/src/sys/cputemp.c	\
-	kernel/src/net/endianess.c \
-	kernel/src/net/cards.c \
-	kernel/src/net/ethernet.c \
-	kernel/src/net/arp.c \
-	kernel/src/net/ipv4.c \
-	kernel/src/net/udp.c \
-	kernel/src/net/dhcp.c \
-	kernel/src/net/icmp.c \
-	kernel/src/sys/system.c \
-	kernel/src/io/status_sounds.c \
-	kernel/src/io/status_loggers.c \
-	kernel/src/extra/cli.c \
-	kernel/src/sys/variable.c	\
-	kernel/src/fs/fat32.c \
-	kernel/src/sys/fxsave_region.c \
-	kernel/src/toys/gfxbench.c \
-	kernel/src/toys/miniplay.c \
-	kernel/src/drv/rtl8139.c \
-	kernel/src/drv/network/virtio_network.c \
-	kernel/src/fmt/tga.c \
-	kernel/src/lib/sprintf.c \
-	kernel/src/debug/ubsan.c \
-	kernel/src/drv/disk/floppy.c \
-	kernel/src/drv/disk/ata_dma.c \
-	kernel/src/drv/audio/ac97.c \
-	kernel/src/sys/elf.c \
-	kernel/src/sys/acpi.c \
-	kernel/src/sys/pixfmt.c \
-	kernel/src/desktop/render.c \
-	kernel/src/desktop/window.c \
-	kernel/src/desktop/widget.c \
-	kernel/src/desktop/widget_button.c \
-	kernel/src/desktop/widget_progress.c \
-	kernel/src/desktop/widget_image.c \
-	kernel/src/desktop/widget_label.c \
-	kernel/src/desktop/eki.c \
-	kernel/src/desktop/parallel_desktop.c \
-	kernel/src/sys/mtrr.c \
-	kernel/src/net/net_info_cli.c \
-	kernel/src/toys/mala.c \
-	kernel/src/debug/memmeter.c \
-	kernel/src/drv/disk/ahci.c \
-	kernel/src/drv/disk/ata_pio.c \
-	kernel/src/toys/minesweeper.c \
-	kernel/src/toys/calendar.c \
-	kernel/src/toys/diskctl.c \
-	kernel/src/lib/utf_conversion.c \
-	kernel/src/lib/base64.c \
-	kernel/src/sys/file_descriptors.c \
-	kernel/src/net/tcp.c \
-	kernel/src/net/stack.c \
-	kernel/src/toys/pavi.c \
-	kernel/src/drv/audio/hda.c \
-	kernel/src/sys/grub_modules.c \
-	kernel/src/drv/disk/mbr.c \
-	kernel/src/sys/lapic.c \
-	kernel/src/drv/ps2.c \
-	kernel/src/gfx/intel.c \
-	$(GAMEBOY) \
-	kernel/src/kernel.c \
+	$(BUILD_PREFIX)kernel/src/drv/psf.c \
+	$(BUILD_PREFIX)kernel/src/sys/unwind.c \
+	$(BUILD_PREFIX)kernel/src/fs/NatSuki.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/initrd.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/dpm.c \
+	$(BUILD_PREFIX)kernel/src/lib/list.c \
+	$(BUILD_PREFIX)kernel/src/lib/fileio.c \
+	$(BUILD_PREFIX)kernel/src/sys/sync.c \
+	$(BUILD_PREFIX)kernel/src/gui/basics.c \
+	$(BUILD_PREFIX)kernel/src/lib/pixel.c \
+	$(BUILD_PREFIX)kernel/src/sys/bootscreen.c \
+	$(BUILD_PREFIX)kernel/src/debug/hexview.c \
+	$(BUILD_PREFIX)kernel/src/drv/video/vbe.c \
+	$(BUILD_PREFIX)kernel/src/drv/input/keyboard.c \
+	$(BUILD_PREFIX)kernel/src/drv/input/mouse.c \
+	$(BUILD_PREFIX)kernel/src/sys/syscalls.c \
+	$(BUILD_PREFIX)kernel/src/sys/testing.c \
+	$(BUILD_PREFIX)kernel/src/sys/trigger.c \
+	$(BUILD_PREFIX)kernel/src/lib/rand.c \
+	$(BUILD_PREFIX)kernel/src/drv/cmos.c \
+	$(BUILD_PREFIX)kernel/src/drv/beeper.c \
+	$(BUILD_PREFIX)kernel/src/user/env.c \
+	$(BUILD_PREFIX)kernel/src/drv/pci.c \
+	$(BUILD_PREFIX)kernel/src/gui/pointutils.c \
+	$(BUILD_PREFIX)kernel/src/gui/line.c \
+	$(BUILD_PREFIX)kernel/src/gui/circle.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/exp.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/log.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/pow.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/acos.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/asin.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/atan.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/modf.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/integral.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/sin.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/cos.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/tan.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/sqrt.c \
+	$(BUILD_PREFIX)kernel/src/io/rgb_image.c \
+	$(BUILD_PREFIX)kernel/src/lib/math/cbrt.c \
+	$(BUILD_PREFIX)kernel/src/sys/cpuid.c	\
+	$(BUILD_PREFIX)kernel/src/drv/disk/ata.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/atapi.c \
+	$(BUILD_PREFIX)kernel/src/sys/cputemp.c	\
+	$(BUILD_PREFIX)kernel/src/net/endianess.c \
+	$(BUILD_PREFIX)kernel/src/net/cards.c \
+	$(BUILD_PREFIX)kernel/src/net/ethernet.c \
+	$(BUILD_PREFIX)kernel/src/net/arp.c \
+	$(BUILD_PREFIX)kernel/src/net/ipv4.c \
+	$(BUILD_PREFIX)kernel/src/net/udp.c \
+	$(BUILD_PREFIX)kernel/src/net/dhcp.c \
+	$(BUILD_PREFIX)kernel/src/net/icmp.c \
+	$(BUILD_PREFIX)kernel/src/sys/system.c \
+	$(BUILD_PREFIX)kernel/src/io/status_sounds.c \
+	$(BUILD_PREFIX)kernel/src/io/status_loggers.c \
+	$(BUILD_PREFIX)kernel/src/extra/cli.c \
+	$(BUILD_PREFIX)kernel/src/sys/variable.c	\
+	$(BUILD_PREFIX)kernel/src/fs/fat32.c \
+	$(BUILD_PREFIX)kernel/src/sys/fxsave_region.c \
+	$(BUILD_PREFIX)kernel/src/toys/gfxbench.c \
+	$(BUILD_PREFIX)kernel/src/toys/miniplay.c \
+	$(BUILD_PREFIX)kernel/src/drv/rtl8139.c \
+	$(BUILD_PREFIX)kernel/src/drv/network/virtio_network.c \
+	$(BUILD_PREFIX)kernel/src/fmt/tga.c \
+	$(BUILD_PREFIX)kernel/src/lib/sprintf.c \
+	$(BUILD_PREFIX)kernel/src/debug/ubsan.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/floppy.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/ata_dma.c \
+	$(BUILD_PREFIX)kernel/src/drv/audio/ac97.c \
+	$(BUILD_PREFIX)kernel/src/sys/elf.c \
+	$(BUILD_PREFIX)kernel/src/sys/acpi.c \
+	$(BUILD_PREFIX)kernel/src/sys/pixfmt.c \
+	$(BUILD_PREFIX)kernel/src/desktop/render.c \
+	$(BUILD_PREFIX)kernel/src/desktop/window.c \
+	$(BUILD_PREFIX)kernel/src/desktop/widget.c \
+	$(BUILD_PREFIX)kernel/src/desktop/widget_button.c \
+	$(BUILD_PREFIX)kernel/src/desktop/widget_progress.c \
+	$(BUILD_PREFIX)kernel/src/desktop/widget_image.c \
+	$(BUILD_PREFIX)kernel/src/desktop/widget_label.c \
+	$(BUILD_PREFIX)kernel/src/desktop/eki.c \
+	$(BUILD_PREFIX)kernel/src/desktop/parallel_desktop.c \
+	$(BUILD_PREFIX)kernel/src/sys/mtrr.c \
+	$(BUILD_PREFIX)kernel/src/net/net_info_cli.c \
+	$(BUILD_PREFIX)kernel/src/toys/mala.c \
+	$(BUILD_PREFIX)kernel/src/debug/memmeter.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/ahci.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/ata_pio.c \
+	$(BUILD_PREFIX)kernel/src/toys/minesweeper.c \
+	$(BUILD_PREFIX)kernel/src/toys/calendar.c \
+	$(BUILD_PREFIX)kernel/src/toys/diskctl.c \
+	$(BUILD_PREFIX)kernel/src/lib/utf_conversion.c \
+	$(BUILD_PREFIX)kernel/src/lib/base64.c \
+	$(BUILD_PREFIX)kernel/src/sys/file_descriptors.c \
+	$(BUILD_PREFIX)kernel/src/net/tcp.c \
+	$(BUILD_PREFIX)kernel/src/net/stack.c \
+	$(BUILD_PREFIX)kernel/src/toys/pavi.c \
+	$(BUILD_PREFIX)kernel/src/drv/audio/hda.c \
+	$(BUILD_PREFIX)kernel/src/sys/grub_modules.c \
+	$(BUILD_PREFIX)kernel/src/drv/disk/mbr.c \
+	$(BUILD_PREFIX)kernel/src/sys/lapic.c \
+	$(BUILD_PREFIX)kernel/src/drv/ps2.c \
+	$(BUILD_PREFIX)kernel/src/gfx/intel.c \
+	$(BUILD_PREFIX)kernel/src/kernel.c \
 #	kernel/src/lib/duktape.c \
 	kernel/src/toys/piano.c \
 	kernel/src/toys/dino.c \
@@ -263,11 +261,17 @@ ifeq ($(SANITIZE),true)
     COMMON_FLAGS := $(COMMON_FLAGS) -fsanitize=undefined
 endif
 
+QEMU_BASE_FLAGS = -cdrom kernel.iso \
+ 				 -m $(MEMORY_SIZE) \
+				 -name "SayoriOS Soul v0.3.5 (Dev)" \
+				 -rtc base=localtime \
+				 -d guest_errors,cpu_reset,int \
+    			 $(KVM_QEMU_FLAGS) \
+	    		 $(ADDITIONAL_QEMU_FLAGS)
+
+
 # NOTE: -d int works only when using tcg accelerator (no KVM)
-QEMU_FLAGS = -cdrom kernel.iso -m $(MEMORY_SIZE) \
-			 -name "SayoriOS Soul v0.3.5 (Dev)" \
-			 -rtc base=localtime \
-			 -d guest_errors,cpu_reset,int \
+QEMU_FLAGS = $(QEMU_BASE_FLAGS) \
 			 -audiodev pa,id=pa0 \
 			 -netdev user,id=net0,net=192.168.111.0,dhcpstart=192.168.111.128,hostfwd=tcp::9999-:9999 \
 			 -device rtl8139,netdev=net0,id=mydev0 \
@@ -277,16 +281,11 @@ QEMU_FLAGS = -cdrom kernel.iso -m $(MEMORY_SIZE) \
 			 -trace "hda*" \
 			 -boot d \
 			 -object filter-dump,id=dump0,netdev=net0,file=netdump.pcap \
-			 $(KVM_QEMU_FLAGS) \
-			 $(ADDITIONAL_QEMU_FLAGS)
  			 # -device AC97 \
 
 
 # NOTE: -d int works only when using tcg accelerator (no KVM)
-QEMU_FLAGS_WSL = -m $(MEMORY_SIZE) \
-			 -name "SayoriOS Soul v0.3.5 (Dev) [WSL]" \
-			 -rtc base=localtime \
-			 -smp 1 \
+QEMU_FLAGS_WSL = $(QEMU_BASE_FLAGS) \
 			 -netdev user,id=net0,net=192.168.111.0,dhcpstart=192.168.111.128 \
 			 -device rtl8139,netdev=net0,id=mydev0 \
 			 -device AC97 \
---
+++ configure.py
@@ -0,0 +1,1 @@
+scorbunny.py
